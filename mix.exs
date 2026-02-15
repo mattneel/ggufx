1644216ed241165288250e906a53056a3a6361b1
Matt Neel <m@neel.codes>
2026-02-15 17:43:21 -0500

    ci: target elixir 1.19 only

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule GGUFX.MixProject do
     [
       app: :ggufx,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "GGUFX",
